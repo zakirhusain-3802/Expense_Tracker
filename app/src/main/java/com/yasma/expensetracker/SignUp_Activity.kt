c4a8095f42cd67522f6addefbb0e91c5a2ba87a3
Zakirhusain kalwani <zakirhussain3802@gmail.com>
2023-07-08 15:25:28 +0530

    Started to implement login

---
+++ app/src/main/java/com/yasma/expensetracker/SignUp_Activity.kt
@@ -0,0 +1,11 @@
+package com.yasma.expensetracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUp_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
